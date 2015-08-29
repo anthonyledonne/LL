8590924dd76306bd1e510a6b6821901165c06e94
anthonyledonne <anthony@anthonyledonne.co>
2015-08-29 09:38:15 -0500

    updated directories for source files

--- global.R
+++ global.R
@@ -1,15 +1,19 @@
 library(shiny)
-source("compileUnitsSoldIntoDataFrame.R")
-source("compileSalesRankIntoDataFrame.R")
+source("~/Dropbox/Data Science Stuff/rfunctions/compileUnitsSoldIntoDataFrame.R")
+source("~/Dropbox/Data Science Stuff/rfunctions/compileSalesRankIntoDataFrame.R")
 
 
 ## HOURLY SALES DATA?? ##
-hourly <- TRUE
+hourly <- FALSE
+
+dataDirectory <- "~/Dropbox/Data Science Stuff/apps/data/"
+unitsSoldDirectory <- paste(dataDirectory, "unitsSold", sep = "")
+salesRankDirectory <- paste(dataDirectory, "salesRank", sep = "")
 
 ######################################
 ########## Units Sold Stuff ##########
 ######################################
-unitsSoldData <- compileUnitsSoldIntoDataFrame("data/unitsSold")
+unitsSoldData <- compileUnitsSoldIntoDataFrame(unitsSoldDirectory)
 unitsSoldData$Sum <- rowSums(unitsSoldData[2:length(unitsSoldData)], na.rm = TRUE)
 unitsSoldTitles <- names(unitsSoldData[2:length(unitsSoldData)])
 unitsSoldData$Week <- as.Date(cut(unitsSoldData$Week.Ending, breaks = "week", start.on.monday = FALSE))
@@ -21,5 +25,5 @@ unitsSoldData$Year <- as.Date(cut(unitsSoldData$Week.Ending, breaks = "year"))
 ######################################
 ######### Amazon Sales Rank ##########
 ######################################
-salesRankData <- compileSalesRankIntoDataFrame("data/salesRank", hourly = hourly)
+salesRankData <- compileSalesRankIntoDataFrame(salesRankDirectory, hourly = hourly)
 salesRankTitles <- names(salesRankData[2:length(salesRankData)])
